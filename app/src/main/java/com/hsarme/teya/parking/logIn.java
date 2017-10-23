c43f0f2418c4bb8e468e07c11e462021505eb6db
user <tya.hsarme@gmail.com>
2017-10-23 09:44:43 +0300

    ..

--- app/src/main/java/com/hsarme/teya/parking/logIn.java
+++ app/src/main/java/com/hsarme/teya/parking/logIn.java
@@ -2,9 +2,11 @@ package com.hsarme.teya.parking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class logIn extends AppCompatActivity {
-
+private EditText etEmail;
+    private EditText etPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
